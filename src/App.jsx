8e9b6ed7d37d02cdfa8fdcb4b528fabc91693083
Roweet1604 <rohtironit1212@gmail.com>
2026-05-17 01:24:07 +0530

    Chatbot added

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Navigation from './components/Navigation';
 import Projects from './components/Projects';
+import Robot from './components/Robot';
 import Skills from './components/Skills';
 import SpaceBackground from './components/SpaceBackground';
+
 
 function App() {
   return (
@@ -20,7 +22,7 @@ function App() {
         <Skills />
         <Projects />
         <Contact />
-        {/* <Robot /> */}
+        <Robot />
       </div>
     </div>
   );
